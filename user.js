45451996b40be435e4d25e4f1078608a532a3897
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-17 14:12:14 -0500

    Updated 'User' entity logic: 'class' now distinguishes between 'initial symbols' and 'available symbols'.

--- user.js
+++ user.js
@@ -1,6 +1,7 @@
 var User = ( function() {
     var users = [];
     var symbols = [ 'o', 'x' ];
+    var availableSymbols = symbols.slice( 0 );
 
     function User( userData ) {
         userData = userData || {};
@@ -12,15 +13,15 @@ var User = ( function() {
         }
 
         // Ensure that num. `User` instances does not exceed num. of available 'symbols' (eg. 'x'/'o').
-        if ( !symbols.length ) {
-            console.error( 'Only 2x `User` instances may be created.' );
+        if ( !availableSymbols.length ) {
+            console.error( 'Only ' + symbols.length + 'x `User` instances may be created.' );
             return this;
         }
 
         // Construct `User` instance.
         this.id = ( users.length + 1 );
         this.name = userData.name || 'John Doe';
-        this.symbol = symbols.shift();
+        this.symbol = availableSymbols.shift();
 
         // Add instance to `user` 'class variable'.
         users.push( this );
