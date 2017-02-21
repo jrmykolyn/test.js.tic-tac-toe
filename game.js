ba3f85e95a43eaf3a5ffebdad43abf781086b4d1
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-21 08:59:46 -0500

    Updated 'play()' method on 'Game' entity: method now iterates over provided 'turns' array, updates internal 'grid'.

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
 var Game = (function() {
+    var currentPlayerIndex = null;
+    var winningPlayer = null;
+
+
     /**
      * ...
      */
@@ -28,6 +32,9 @@ var Game = (function() {
         this.players = gameData.players;
         this.grid = gameData.grid;
 
+        // Set/update local vars.
+        currentPlayerIndex = 0;
+
         // Return instance.
         return this;
     } // /Game()
@@ -37,10 +44,32 @@ var Game = (function() {
      * ...
      */
     Game.prototype.play = function( turns ) {
+        // ...
+        var _this = this;
+
+        // Validate presence/type of `turns`.
         if ( !turns || !Array.isArray( turns ) || !turns.length ) {
             console.error( '`play()` method called with missing or invalid `turns` data.' );
+
             return null;
         }
+
+        // Loop over `turns`.
+        turns.forEach( function( turn ) {
+            if ( !winningPlayer ) {
+
+                _this.grid.update( _this.players[ currentPlayerIndex ], turn );
+
+                /// TODO[@jrmykolyn] - Consider moving `checkWin()` method to `Game` prototype.
+                winningPlayer = _this.grid.checkWin(); /// TEMP - Function currently returns `null` in all cases.
+
+                // Update `currentPlayerIndex`.
+                currentPlayerIndex = ( currentPlayerIndex === 1 ) ? 0 : 1;
+            }
+        } );
+
+        /// TODO[@jrmykolyn] - Consider returning winning `User` or ID.
+        return null; /// TEMP
     } // /play()
 
 
